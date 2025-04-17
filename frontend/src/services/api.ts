3bbc9614cf6f327b894c56577ecb653cd25e69d5
Mahir Ramani <22ce105@charusat.edu.in>
2025-04-17 12:22:05 +0530

    deploy [err-VITE_env]

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { UpdateDetailsInput } from '../validation/schemas';
 
-const API_URL =  import.meta.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const API_URL =  import.meta.env.VITE_BACKEND_API_URL || 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
