c83c37f2904490fb387b02b6de793ee05cbb2e5e
Mahir Ramani <22ce105@charusat.edu.in>
2025-04-27 12:30:29 +0530

    db_url

--- backend/studentverify/studentverify/settings.py
+++ backend/studentverify/studentverify/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,11 +121,13 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'Mahir@PSQL',
-        'HOST': 'localhost',
+        'HOST': '',
         'PORT': '5432',
     }
 }
 
+database_url = os.environ.get('DATABASE_URL')
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
